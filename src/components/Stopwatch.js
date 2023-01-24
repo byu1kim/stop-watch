4d628af08074feb8cc96dc6b0ee55612d1cb2cfb
Byul Kim <caxbelle@gmail.com>
2023-01-23 23:08:19 -0800

    watch title

--- src/components/Stopwatch.js
+++ src/components/Stopwatch.js
@@ -25,6 +25,10 @@ const Stopwatch = () => {
     return () => clearInterval(interval);
   }, [running]);
 
+  useEffect(() => {
+    document.title = time;
+  }, [time]);
+
   const handleStart = () => {
     setRunning(true);
   };
@@ -53,11 +57,13 @@ const Stopwatch = () => {
       date.getFullYear() + "-" + (date.getMonth() + 1) + "-" + date.getDate();
     dispatch(save({ date: today, time: time }));
   };
+
   //save btn안눌러도 자동으로 저장되게 하기
   // state사용해서 버튼 사라졋다 안사라졋다
   // 슬라이스 안써도 로컬스토리지만 사용해도 되지않나 ?
   // 스타트 버튼 누를때 데이 저장 ?
   // 화면 넘기면 안돌아가는거 해결
+  // 로컬스토리지에 현재값도 저장, 버튼 안눌러도, 스타트 버튼 누르는 즉시
   return (
     <div className="stopwatch">
       <div className="time-frame">
